ea9f2877c841ac534b9573821afe4657faeab851
Raveenapenagalapati <149499685+Raveenapenagalapati@users.noreply.github.com>
2023-10-31 18:50:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('MY parents new healty dinner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & Blueberry')
+streamlit.text('kale 3 spinach')
+streamlit.text('Boiled EGGS')
